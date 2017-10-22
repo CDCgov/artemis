0da761a83feb3073fa9329ff8ba4b312c8d44090
Allan Reyes <allanbreyes@gmail.com>
2017-10-22 10:02:34 -0700

    Add ApplicationService base class

---
+++ spec/services/application_service_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+class FizzBuzzService < ApplicationService
+  def call!(num)
+    raise ArgumentError, 'only integers' unless num.is_a? Integer
+    text = ''
+    text << 'Fizz' if num % 3 == 0
+    text << 'Buzz' if num % 5 == 0
+    text = num.to_s if text.empty?
+    text
+  end
+end
+
+RSpec.describe ApplicationService, type: :service do
+  it 'raises errors for abstract class method' do
+    error = NotImplementedError
+    expect { described_class.call! }.to raise_error(error)
+    expect { described_class.call }.to raise_error(error)
+    expect { subject.call! }.to raise_error(error)
+    expect { subject.call }.to raise_error(error)
+  end
+
+  context 'when inherited by a valid subclass' do
+    let(:subclass) { FizzBuzzService }
+
+    describe '.call!' do
+      it 'calls the default instance #call! method' do
+        expect(subclass.call!(3)).to eq 'Fizz'
+      end
+
+      it 'raises on error' do
+        expect { subclass.call! 42.0 }.to raise_error(ArgumentError)
+      end
+    end
+
+    describe '.call' do
+      it 'calls the default instance #call method' do
+        expect(subclass.call(5)).to eq 'Buzz'
+      end
+
+      it 'does not raise on error' do
+        expect { subclass.call 42.0 }.to_not raise_error(ArgumentError)
+      end
+    end
+  end
+end
