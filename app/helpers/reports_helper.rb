16a73f1d0f40420c3721686798d6138dc136e40b
Allan Reyes <allanbreyes@gmail.com>
2017-10-28 13:27:07 -0700

    Clean up table

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,9 +1,8 @@
 module ReportsHelper
-  def render_newborn_record(record, emphasized = [])
+  def render_newborn_record(record, fields = [])
     return '[none found]' if record.blank?
-    record.attributes.except(:state_file_number, :id).map do |attribute, value|
-      line = "#{attribute}:\t#{value || '[blank]'}"
-      emphasized.include?(attribute.to_s) ? "<strong>#{line}</strong>" : line
-    end.sort.join("\n")
+    record.attributes.except(:id).map do |attribute, value|
+      (value || '<em>[blank]</em>').to_s if fields.include? attribute.to_s
+    end.compact.join("\n")
   end
 end
