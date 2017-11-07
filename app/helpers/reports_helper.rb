7af723f6f2b857714eeada6ffde682644369eca6
Allan Reyes <areyes37@gatech.edu>
2017-11-07 10:52:29 -0800

    Implement initial sorting fix

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,7 +1,9 @@
 module ReportsHelper
   def render_newborn_record(record, fields = [])
     return '[none found]' if record.blank?
-    record.attributes.except(:id).map do |attribute, value|
+    record.attributes.except(:id).sort_by do |attribute, _value|
+      attribute
+    end.map do |attribute, value|
       (value || '<em>[blank]</em>').to_s if fields.include? attribute.to_s
     end.compact.join("\n")
   end
