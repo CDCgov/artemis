bb65611f6f760d9d6be3658fa4672a04d0ea4256
Reed Allred <reed.allred@gatech.edu>
2017-11-19 15:17:48 -0700

    Adding in erroring handling if csv file does not have the right headers

--- app/services/process_csv_upload_service.rb
+++ app/services/process_csv_upload_service.rb
@@ -1,8 +1,8 @@
 # FIXME: this service mutates class variables in the subsequent CSV records. It
 # will likely break down for concurrent requests.
+require "csv"
 class ProcessCsvUploadService < ApplicationService
   attr_reader :nbs_data, :ovrs_data
-
   def call!(datasets = { nbs: nil, ovrs: nil })
     @nbs_data, @ovrs_data = datasets[:nbs], datasets[:ovrs]
 
@@ -18,12 +18,14 @@ class ProcessCsvUploadService < ApplicationService
   private
 
   def validate_nbs!
-    # TODO: this should validate that uploaded NBS data has all the headers
-    raise 'NBS data not present' unless @nbs_data
+    @nbs_column_names = ["NAME","BABYS_LAST_NAME","BABYS_FIRST_NAME","BIRTHDATE","BABYS_SEX","MOTHERS_LAST_NAME","MOTHERS_BIRTHDATE","BIRTH_WEIGHT","MULTIPLE_BIRTH","BIRTH_LENGTH"]
+    @nbs_data_columns = CSV.parse(@nbs_data)[0]
+    raise 'NBS data not present' unless @nbs_data_columns.sort == @nbs_column_names.sort#.subset? @main_set
   end
 
   def validate_ovrs!
-    # TODO: this should validate that uploaded OVRS data has all the headers
-    raise 'OVRS data not present' unless @ovrs_data
+    @ovrs_column_names = ["StateFileNumber","NewbornScreeningNumber","ChildLast","ChildFirst","BirthCCYY","BirthMM","BirthDD","Gender","MomLast","MomBirthCCYY","MomBirthMM","MomBirthDD","BirthWeightGrams","Plurality","ChildLengthCm"]
+    @ovrs_data_columns = CSV.parse(@ovrs_data)[0]
+    raise 'OVRS data not present' unless @ovrs_data_columns.sort == @ovrs_column_names.sort
   end
 end
